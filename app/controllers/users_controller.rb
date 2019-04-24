083edf6911e6b17da59e42f51df1a2e2d0f22307
Sunny <ring2rings93@gmail.com>
2019-04-24 23:40:46 +0100

    get methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def index
+    @name = 'This is the index method'
+  end
+
+  def show
+    @show = 'This is the Show action method'
+  end
+
+  def edit
+    @edit = 'This is the Edit action method'
+  end
+
+  def new
+    @new = 'This is the New action method'
+  end
+
+
 end
