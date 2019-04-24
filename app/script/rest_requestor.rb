3385aae22ebdee1e7b8bd19a665319548b94586f
Sunny <ring2rings93@gmail.com>
2019-04-24 23:54:24 +0100

    routes,views and contoller actions

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -2,4 +2,7 @@ require 'rest-client'
 url = 'http://localhost:3000'
 
 p RestClient.get(url)
-puts RestClient.get(url + '/users')
+p RestClient.get(url + '/users')
+p RestClient.get(url + '/users/1')
+p RestClient.get(url + '/users/new')
+p RestClient.get(url + '/users/1/edit')
